34fc54ff6e46d177eabfe44fd00366acf3a75776
TIKI-developer <117838226+TIKI-developer@users.noreply.github.com>
2024-12-10 21:27:47 +0800

    Update UserController.cs

--- Restaurant.WebApi/Controllers/UserController.cs
+++ Restaurant.WebApi/Controllers/UserController.cs
@@ -166,9 +166,13 @@ namespace Restaurant.WebApi.Controllers
         private string NormalizePhoneNumber(string? phoneNumber)
         {
             if (string.IsNullOrEmpty(phoneNumber)) return "";
-            if (phoneNumber.StartsWith("89") || phoneNumber.StartsWith("+7"))
+            if (phoneNumber.StartsWith("89"))
             {
                 return string.Concat("7", phoneNumber.AsSpan(1));
+            }
+            if (phoneNumber.StartsWith("+79"))
+            {
+                return string.Concat("7", phoneNumber.AsSpan(2));
             }
             return phoneNumber;
         }
