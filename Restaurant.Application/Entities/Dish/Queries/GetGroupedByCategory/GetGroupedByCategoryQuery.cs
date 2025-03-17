aa690af7a97730d8ad74ab50684076b359fba9f2
TIKI-developer <117838226+TIKI-developer@users.noreply.github.com>
2025-03-17 23:26:04 +0800

    Added /dishes/categories endpoint
    This endpoints gets ViewModel with Dictionary of categoryId and his dishes

---
+++ Restaurant.Application/Entities/Dish/Queries/GetGroupedByCategory/GetGroupedByCategoryQuery.cs
@@ -0,0 +1,7 @@
+﻿using MediatR;
+using Restaurant.Application.ViewModels;
+
+namespace Restaurant.Application.Entities.Dish.Queries.GetGroupedByCategory
+{
+    public class GetGroupedByCategoryQuery : IRequest<DishListGroupedByCategory> { }
+}
---
+++ Restaurant.Application/Entities/Dish/Queries/GetGroupedByCategory/GetGroupedByCategoryQueryHandler.cs
@@ -0,0 +1,42 @@
+﻿using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Restaurant.Application.Interfaces;
+using Restaurant.Application.ViewModels;
+
+namespace Restaurant.Application.Entities.Dish.Queries.GetGroupedByCategory
+{
+    public class GetGroupedByCategoryQueryHandler
+        (IRestaurantDbContext dbContext,
+        IMapper mapper)
+        : IRequestHandler<GetGroupedByCategoryQuery, DishListGroupedByCategory>
+    {
+        private readonly IRestaurantDbContext _dbContext = dbContext;
+        private readonly IMapper _mapper = mapper;
+
+        public async Task<DishListGroupedByCategory> Handle(GetGroupedByCategoryQuery request, CancellationToken cancellationToken)
+        {
+            var categories = await 
+                _dbContext
+                .Categories
+                .Where(e => e.Content.IsPublished == true)
+                .AsNoTracking()
+                .ToListAsync(cancellationToken);
+
+            var vm = new DishListGroupedByCategory();
+
+            foreach (var category in categories)
+            {
+                var categoryDishes = await _dbContext.Dishes
+                     .Where(d => d.Categories.Any(c => c.Id == category.Id))
+                     .ProjectTo<DishLookup>(_mapper.ConfigurationProvider)
+                     .AsNoTracking()
+                     .ToListAsync();
+                vm.CategoriesDishes.Add(category.Id, categoryDishes);
+            }
+
+            return vm;
+        }
+    }
+}
