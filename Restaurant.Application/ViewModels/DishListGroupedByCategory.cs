7f2ba4a063928cc0b5df43dce973a185fa5580f2
TIKI-developer <117838226+TIKI-developer@users.noreply.github.com>
2025-03-17 23:59:41 +0800

    Fixes

--- Restaurant.Application/ViewModels/DishListGroupedByCategory.cs
+++ Restaurant.Application/ViewModels/DishListGroupedByCategory.cs
@@ -2,6 +2,12 @@
 {
     public class DishListGroupedByCategory
     {
-        public Dictionary<Guid, ICollection<DishLookup>> CategoriesDishes { get; set; } = [];
+        public List<CategoryDishesDto> CategoriesDishes { get; set; } = [];
+    }
+
+    public class CategoryDishesDto
+    {
+        public required Guid CategoryId { get; set; }
+        public ICollection<DishLookup>? Dishes { get; set; }
     }
 }
